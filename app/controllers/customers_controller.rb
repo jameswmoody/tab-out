b95e037a3c0e830f57452da350c13d06d5655f7a
Evan Hickle <ehickle@alumni.nd.edu>
2017-07-28 18:33:57 -0500

    no change

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,7 +2,7 @@ class CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
-    @recent_tabs = @customer.tabs.order("DESC").limit(3)
+    @recent_tabs = @customer.tabs.order("updated_at DESC").limit(3)
     @open_tab = @customer.tabs.last
     @bar = @open_tab.business.dba
     p @bar
