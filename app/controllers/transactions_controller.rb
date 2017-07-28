cde24b4b08ec7be99ccdfb530bff4d4efe2562bb
James Moody <email": null>
2017-07-28 18:59:16 -0500

    Remove p statements and add association that was deleted

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -10,13 +10,9 @@ class TransactionsController < ApplicationController
   def create
     @customer = Customer.new({username: 'nico', first_name:'nico', last_name: 'glennon', email: 'n@n.com', phone: '1234567', password_digest: '123456', vault_id: '123456'})
 
-    p @customer
-
     payment_params = params[:transaction]
 
     result = CreditCardService.new({customer: @customer}).create_transaction('57.12')
-
-    p result
 
     redirect_to '/transactions/new'
   end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
   has_secure_password
 
+  has_many :tabs
+
   validates :username, :phone, :first_name, :last_name, :email, presence: true
   validates :username, :phone, :email, uniqueness: true
 
