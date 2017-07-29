75a25b660b30434b7b11645187d7d74b157d1694
Josh Harvey <jmharvey0@gmail.com>
2017-07-29 13:07:47 -0500

    make changes to tab partial and referenece to tab partial to make it work better

--- app/helpers/tabs_helper.rb
+++ app/helpers/tabs_helper.rb
@@ -1,14 +1,10 @@
 module TabsHelper
-  def price_in_dollars(cents)
-    dollars = cents/100
-    "$#{dollars.to_s}"
-  end
-
-  def tabs_view(tab)
-    if @business
-      "#{tab.customer.first_name}....#{price_in_dollars(tab.total_price)}"
-    else
-      "#{tab.business.dba}....#{price_in_dollars(tab.total_price)}"
+  def price_in_dollars(price)
+    dollars = price/100
+    cents = price%100
+    if cents < 10
+      cents = "0#{cents}"
     end
+    "$#{dollars}.#{cents}"
   end
 end
