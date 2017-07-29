87117c719da48f13679bcc093dee26499ccac88b
Josh Harvey <jmharvey0@gmail.com>
2017-07-29 15:58:19 -0500

    small authoriztion change to business show

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,7 +1,7 @@
 class BusinessesController < ApplicationController
   def show
     @business = Business.find(params[:id])
-    redirect_to root_path unless session[:user_id] == @business.id
+    redirect_to root_path unless session[:user_id] == @business.id && session[:user_type] == "#{@business.class}"
     if params[:search]
       @tabs = @business.search(params[:search])
     else
