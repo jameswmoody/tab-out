6593aa5ba8ce0f4ee64cb6f89ae6d8d12f82fcac
Nico Glennon <nicoglennon@icloud.com>
2017-07-29 12:30:34 -0500

    Add james' user auth controller methods that did not tranfer in the merge

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,4 +5,25 @@ class CustomersController < ApplicationController
     @recent_tabs = @customer.tabs.order("updated_at DESC").limit(3)
     @open_tab = @customer.tabs.last
   end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save && params[:customer][:password] == params[:customer][:password_confirmation]
+      login @customer
+      redirect_to cards_new_path
+    else
+      @errors = @customer.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:username, :password, :password_confirmation, :first_name, :last_name, :email, :phone)
+  end
 end
