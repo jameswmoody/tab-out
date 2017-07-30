a1948207107c3817fd24ac7b7ab17b0b138a9494
James W Moody <jamesmoodyradio@gmail.com>
2017-07-29 23:39:15 -0500

    Add js to prevent links from opening safari on iOS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,4 +27,12 @@ $( document ).ready(function() {
       $('#total-price-checkout').text('$'+ totalInt);
   });
 
-})
+  var a=document.getElementsByTagName("a");
+
+  for(var i=0;i<a.length;i++) {
+    a[i].onclick=function() {
+        window.location=this.getAttribute("href");
+        return false
+    }
+  }
+});
