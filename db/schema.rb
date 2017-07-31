a2aa08a257897262d71fec3592e2bfc132296604
Evan <ehickle@alumni.nd.edu>
2017-07-31 10:04:51 -0500

    Run migration for adding tip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170730015901) do
+ActiveRecord::Schema.define(version: 20170731144815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20170730015901) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "transaction_id"
+    t.integer "tip"
     t.index ["business_id"], name: "index_tabs_on_business_id"
     t.index ["customer_id"], name: "index_tabs_on_customer_id"
   end
