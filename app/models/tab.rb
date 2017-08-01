cc4eed7fe402af78a3b16867b203f7cf958becba
Evan Hickle <ehickle@alumni.nd.edu>
2017-08-01 10:49:06 -0500

    Comment out custom validation in tab.rb

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -3,7 +3,7 @@ class Tab < ApplicationRecord
   belongs_to :business
   has_many :items, dependent: :destroy
   validates :customer_id, :business_id, presence: true
-  validate :customer_one_open_tab_at_bar
+  # validate :customer_one_open_tab_at_bar
 
   def total_price
     sub = self.items.reduce(0) { |total, item| total + item.price }
@@ -13,12 +13,12 @@ class Tab < ApplicationRecord
     self.transaction_id == nil
   end
 
-  def customer_one_open_tab_at_bar
-    business = self.business
-    match = business.tabs.find_by(business_id: self.business_id, customer_id: self.customer_id)
-    p match
-    if match && match.is_open?
-      errors.add(:unique, "You already have an open tab with this bar")
-    end
-  end
+  # def customer_one_open_tab_at_bar
+  #   business = self.business
+  #   match = business.tabs.find_by(business_id: self.business_id, customer_id: self.customer_id)
+  #   p match.is_open?
+  #   if match && match.is_open?
+  #     errors.add(:unique, "You already have an open tab with this bar")
+  #   end
+  # end
 end
